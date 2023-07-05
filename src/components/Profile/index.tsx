674d450b832796bc68fe789a1480d0450276cbc8
unknown <vhisoares39@gmail.com>
2023-07-04 21:18:17 -0300

    Add: Profile content

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,4 +1,9 @@
-import { ProfileContainer, ProfileContent } from "./styles";
+import { ProfileContainer, ProfileContent, Button} from "./styles";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare, faBuilding, faUserGroup} from '@fortawesome/free-solid-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+
 
 export function Profile(){
     return (
@@ -9,7 +14,13 @@ export function Profile(){
             <ProfileContent>
                 <div>
                     <h2>Vinicius Soares</h2>
-                    <button>GitHub </button>
+                    <Button href="https://github.com/Vinicius-SO"><span>GITHUB</span> <FontAwesomeIcon icon={faArrowUpRightFromSquare}/> </Button>
+                </div>
+                <div>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</div>
+                <div className="infos">
+                    <span><FontAwesomeIcon icon={faGithub}/>Vinicius-SO</span>
+                    <span><FontAwesomeIcon icon={faBuilding}/>Sollut</span>
+                    <span><FontAwesomeIcon icon={faUserGroup}/>32 seguidores</span>
                 </div>
                                 
             </ProfileContent>
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -5,17 +5,15 @@ export const ProfileContainer = styled.div`
     height: 13.25rem;
 
     display: flex;
+    align-items: center;
     flex-direction: row;
 
     top: 50%;
     left: 50%;
 
     transform: translate(-50%, -50%);
-    /* margin: -5% auto; */
 
     position: relative;
-
-    /* z-index: 2; */
 
     border-radius: 0.625rem;
     background: ${props=>props.theme.profile};
@@ -31,15 +29,44 @@ export const ProfileContainer = styled.div`
 
 export const ProfileContent = styled.div`
     width: 100% ;
-    &> div{
-        display: flex;
-        justify-content: space-between;
-
-        width: 100%;
-    }
-
-
-
     margin: 2rem;
 
+    div:first-child{
+        display: flex;
+        
+        justify-content: space-between;
+        align-items: center;
+        
+        width: 100%;
+        margin-bottom: 0.5rem;
+    }
+
+    .infos{
+        margin-top: 1.5rem;
+        
+        display: flex;
+        gap: 1.5rem;
+        & > span{
+            display: flex;
+            gap: 0.5rem;
+            align-items: center;
+        }
+    }
+
+`
+
+export const Button = styled.a`
+    align-items: center;
+    font-size: 12px;
+    font-weight: 160 ;
+    text-decoration: none;
+    text-transform: capitalize;
+
+    background: transparent ;
+    border: 0;
+    color: ${props=> props.theme["brand-blue"]} ;
+
+    & > span{
+        margin-right: 0.5rem
+    }
 `
